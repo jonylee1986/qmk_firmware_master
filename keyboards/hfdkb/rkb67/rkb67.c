8a099d5fbc1ae7fac0956326df66f6aedc30c0bf
galican <2831039915@qq.com>
2025-12-10 23:02:40 +0800

    added rkb67

---
+++ keyboards/hfdkb/rkb67/rkb67.c
@@ -0,0 +1,160 @@
+// Copyright 2023 JoyLee (@itarze)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "common/bt_task.h"
+#include "usb_main.h"
+
+bool led_inited = false;
+
+void led_config_all(void) {
+    if (!led_inited) {
+        // Set our LED pins as output
+        setPinOutput(RGB_DRIVER_SDB_PIN);
+        writePinLow(RGB_DRIVER_SDB_PIN);
+        led_inited = true;
+    }
+}
+
+void led_deconfig_all(void) {
+    if (led_inited) {
+        // Set our LED pins as input
+        setPinOutput(RGB_DRIVER_SDB_PIN);
+        writePinHigh(RGB_DRIVER_SDB_PIN);
+        led_inited = false;
+    }
+}
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (process_record_user(keycode, record) != true) {
+        return false;
+    }
+
+#ifdef BT_MODE_ENABLE
+    if (process_record_bt(keycode, record) != true) {
+        return false;
+    }
+#endif
+
+    switch (keycode) {
+        case RM_TOGG:
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        rgb_matrix_set_flags(LED_FLAG_NONE);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                    } break;
+                    default: {
+                        rgb_matrix_set_flags(LED_FLAG_ALL);
+                    } break;
+                }
+            }
+            if (!rgb_matrix_is_enabled()) {
+                rgb_matrix_set_flags(LED_FLAG_ALL);
+                rgb_matrix_enable();
+            }
+            return false;
+        default:
+            break;
+    }
+
+    return true;
+}
+
+void matrix_init_kb(void) {
+#ifdef BT_MODE_ENABLE
+    bt_init();
+    led_config_all();
+#endif
+    matrix_init_user();
+}
+
+void matrix_scan_kb(void) {
+#ifdef BT_MODE_ENABLE
+    bt_task();
+#endif
+    matrix_scan_user();
+}
+
+void housekeeping_task_kb(void) {
+#ifdef BT_MODE_ENABLE
+    extern void housekeeping_task_bt(void);
+    housekeeping_task_bt();
+#endif
+
+#ifdef CONSOLE_ENABLE
+    debug_enable = true;
+#endif
+
+    static uint32_t usb_suspend_timer = 0;
+    static uint32_t usb_suspend       = false;
+
+    if (dev_info.devs == DEVS_USB) {
+        if (USB_DRIVER.state != USB_ACTIVE || USB_DRIVER.state == USB_SUSPENDED) {
+            if (!usb_suspend_timer) {
+                usb_suspend_timer = timer_read32();
+            } else if (timer_elapsed32(usb_suspend_timer) > 10000) {
+                if (!usb_suspend) {
+                    usb_suspend = true;
+                    led_deconfig_all();
+                }
+                usb_suspend_timer = 0;
+            }
+        } else {
+            if (usb_suspend_timer) {
+                usb_suspend_timer = 0;
+                if (usb_suspend) {
+                    usb_suspend = false;
+                    led_config_all();
+                }
+            }
+        }
+    } else {
+        if (usb_suspend) {
+            usb_suspend_timer = 0;
+            usb_suspend       = false;
+            led_config_all();
+        }
+    }
+}
+
+#ifdef RGB_MATRIX_ENABLE
+void blink_rgb_advanced(void);
+
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_get_flags()) {
+        rgb_matrix_set_color_all(RGB_OFF);
+    }
+
+    if (!readPin(BT_CABLE_PIN)) {
+        for (uint8_t i = 68; i < 74; i++) {
+            rgb_matrix_set_color(i, 100, 100, 100);
+        }
+    } else {
+        for (uint8_t i = 68; i < 74; i++) {
+            rgb_matrix_set_color(i, RGB_OFF);
+        }
+    }
+
+    if (rgb_matrix_indicators_advanced_user(led_min, led_max) != true) {
+        return false;
+    }
+
+#    ifdef BT_MODE_ENABLE
+    if (bt_indicator_rgb(led_min, led_max) != true) {
+        return false;
+    }
+#    endif
+
+    // caps lock red
+    if ((host_keyboard_led_state().caps_lock) && ((bts_info.bt_info.paired) || (dev_info.devs == DEVS_USB))) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 100, 100, 100);
+    }
+    // GUI lock red
+    if (keymap_config.no_gui) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(GUI_LOCK_LED_INDEX, 100, 100, 100);
+    }
+
+    return true;
+}
+#endif
