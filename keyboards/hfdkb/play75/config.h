7fd403eaf276b47c589791e9836637993192554b
galican <2831039915@qq.com>
2026-01-09 18:34:05 +0800

    Updated hfdkb/m91, updated hfdkb/m89u, 2026/01/09

---
+++ keyboards/hfdkb/play75/config.h
@@ -0,0 +1,43 @@
+// Copyright 2023 JoyLee (@itarze)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+#ifdef BT_MODE_ENABLE
+#    define BT_CABLE_PIN B8
+#    define BT_CHARGE_PIN B9
+#    define RGB_DRIVER_SDB_PIN A15
+#    define USB_SUSPEND_CHECK_ENABLE
+// #  define BT_MODE_SW_PIN C13 // 低电平时
+// #  define RF_MODE_SW_PIN C14 // 低电平时
+// #  define LED_CAPS_LOCK_PIN C10
+// #  define LED_WIN_LOCK_PIN C11
+// #  define LED_MAC_MODE_PIN D2
+#endif
+
+// #define USB_SUSPEND_CHECK_ENABLE
+
+/* I2C Config for LED Driver */
+#define SNLED27351_I2C_ADDRESS_0 SNLED27351_I2C_ADDRESS_GND
+#define SNLED27351_I2C_ADDRESS_1 SNLED27351_I2C_ADDRESS_VDDIO
+
+#define I2C1_OPMODE OPMODE_I2C
+#define I2C1_CLOCK_SPEED 1000000
+
+/* SPI Config for spi flash*/
+#define SPI_DRIVER SPIDQ
+#define SPI_SCK_PIN B3
+#define SPI_MOSI_PIN B5
+#define SPI_MISO_PIN B4
+#define SPI_MOSI_PAL_MODE 5
+
+#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
+
+#define DYNAMIC_KEYMAP_MACRO_DELAY 50
+
+#define KEY_NUM 8
