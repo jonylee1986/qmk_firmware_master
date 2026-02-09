d6a4b7afaaca47e45477453b0ed3bb258b4668af
galican <2831039915@qq.com>
2026-02-09 18:45:25 +0800

    Udpated rkb68, 2026/02/09

--- keyboards/0_hfdkb/rkb68/config.h
+++ keyboards/0_hfdkb/rkb68/config.h
@@ -8,23 +8,13 @@
  *  These options are also useful to firmware size reduction.
  */
 
-#define BT_MODE_ENABLE
-
 #ifdef BT_MODE_ENABLE
-#  define NO_USB_STARTUP_CHECK
-#  define ENTRY_STOP_MODE // 超时进入STOP Mode
-#  define BT_CABLE_PIN B8 // 充电接入时为高
-#  define BT_CHARGE_PIN B9 // 充电时为低，充满时为高
-#  define WS2812_EN_PIN B7 // WS2812电源
-#  define BT_MODE_SW_PIN C13 // 低电平时
-#  define RF_MODE_SW_PIN C14 // 低电平时
-
+#    define BT_CABLE_PIN B8  // 充电接入时为高
+#    define BT_CHARGE_PIN B9 // 充电时为低，充满时为高
+#    define WS2812_EN_PIN B7 // WS2812电源
 #endif
 
 /* RGB Matrix */
-#define RGB_DI_PIN C15
-#define RGB_MATRIX_LED_COUNT 11
-#define RGB_DISABLE_WHEN_USB_SUSPENDED
 
 /* SPI Config for spi flash*/
 #define SPI_DRIVER SPIDQ
@@ -34,7 +24,3 @@
 #define SPI_MOSI_PAL_MODE 5
 
 #define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
-#define WEAR_LEVELING_BACKING_SIZE (4 * 1024)
-
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define RGB_MATRIX_KEYPRESSES
